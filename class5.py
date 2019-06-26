57cc45575827af84ec309456234e1fe3fbccdf29
manankshastri <manank.shastri@gmail.com>
2019-06-26 14:29:25 -0400

    class - reverse string

---
+++ class5.py
@@ -0,0 +1,10 @@
+class RevStr(str):
+    def __str__(self):
+        return self[::-1]
+
+def main():
+    hello = RevStr('Hello, World!!')
+    print(hello)
+
+if __name__ == '__main__':
+    main()
